286d2fdaee6c81d6fcee15b1378d5fc1f430d384
Seunghwan Shin <shinn92kr@gmail.com>
2021-08-08 06:25:20 +0900

    Update quotes.js
    Using quotes API

--- js/quotes.js
+++ js/quotes.js
@@ -1,47 +1,17 @@
-const quotes = [
-    {
-        quote: "The road to success and the road to failure are almost exactly the same.",
-        author: "Colin R. Davis",
-    },
-    {
-        quote: "It is better to fail in originality than to succeed in imitation.",
-        author: "Herman Melville",
-    },
-    {
-        quote: "Success is walking from failure to failure with no loss of enthusiasm.",
-        author: "Winston Churchill",
-    },
-    {
-        quote: "All progress takes place outside the comfort zone.",
-        author: "Michael John Bobak",
-    },
-    {
-        quote: "Success usually comes to those who are too busy to be looking for it.",
-        author: "Henry David Thoreau",
-    },
-    // {
-    //     quote: "",
-    //     author: "",
-    // },
-    // {
-    //     quote: "",
-    //     author: "",
-    // },
-    // {
-    //     quote: "",
-    //     author: "",
-    // },
-    // {
-    //     quote: "",
-    //     author: "",
-    // },
-];
+function randomQuote() {
+    const apiUrl = "https://goquotes-api.herokuapp.com/api/v1/random?count=1";
+    fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            const quoteSpan = document.querySelector("#quote span:first-child");
+            const authorSpan = document.querySelector("#quote span:last-child");
 
-const quote = document.querySelector("#quote span:first-child");
-const author = document.querySelector("#quote span:last-child");
+            const quote = data.quotes[0].text;
+            const author = data.quotes[0].author;
 
-const quotesLen = quotes.length;
-const ran = Math.floor(Math.random() * quotesLen);
+            quoteSpan.innerText = quote;
+            authorSpan.innerText = author;
+        });
+}
 
-quote.innerText = quotes[ran].quote;
-author.innerText = quotes[ran].author;
+randomQuote();
